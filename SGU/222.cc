1bb90936ed96e300bda134c460c4aa0d04d8e487
Unisolate <unisolate@gmail.com>
2014-08-04 19:56:52 +0800

    sgu

--- SGU/222.cc
+++ SGU/222.cc
@@ -1,8 +1,19 @@
+//简化版n皇后 不需考虑斜行
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-	int n,k;
-	scanf("%d%d",&n,&k);
-	return 0;
+    int n, k;
+    long long ans = 0;
+    scanf("%d%d", &n, &k);
+    if (k <= n)
+    {
+        ans = 1;
+        for (int i = n - k + 1; i <= n; ++i)
+            ans *= (i * i);
+        for (int i = 1; i <= k; ++i)
+            ans /= i;
+    }
+    printf("%lld\n", ans);
+    return 0;
 }
