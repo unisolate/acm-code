1dc441480fa158df576b148fb2a47d84fc7bc4f2
unisolate <unisolate@gmail.com>
2014-11-27 21:07:34 +0800

    daily

--- Weekly/20141125/I.cc
+++ Weekly/20141125/I.cc
@@ -1,22 +1,38 @@
 #include <cstdio>
+#include <cstring>
+#include <map>
+#include <string>
 using namespace std;
+struct team
+{
+    int pt, ac;
+    int pb[30];
+    string tn;
+} x[50050];
+int cmp(team a, team b)
+{
+    if (a.ac == b.ac)
+        return a.pt < b.pt;
+    return a.ac < b.ac;
+}
 int main()
 {
-    int n;
-    long long ans = 1;
-    for (int i = 1; i <= 500; ++i)
-        ans = ans * (500 + i) / i;
-    printf("%d\n", (1 ^ 2 ^ 10) + (1 ^ 2 ^ 1) + (1 ^ 10 ^ 1) + (2 ^ 10 ^ 1));
-    for (int i = 0; i <= 10; ++i)
+    int t, cas = 0, n, m, p, q;
+    scanf("%d", &t);
+    while (t--)
     {
-        for (int j = 0; j <= 10; ++j)
-            printf("%4d", i ^ j);
-        putchar(10);
-    }
-    while (~scanf("%d", &n))
-    {
-        // for (int i = 0; i < n; ++i)
-        // scanf("%d", &c[i]);
+        map<string, int> mp;
+        scanf("%d%d%d%d", &n, &m, &p, &q);
+        for (int i = 0; i < n; ++i)
+        {
+            scanf("%s%s%d%s");
+        }
+        printf("Case #%d:\n", ++cas);
+        sort(x, x + mp.size(), cmp);
+        for (int i = 0; i < mp.size(); ++i)
+        {
+            printf("%s %d ", x[i].tn, i + 1);
+        }
     }
     return 0;
 }
