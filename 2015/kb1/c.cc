e273894e37e076eccfa21b40bfe121837adef12a
unisolate <unisolate@gmail.com>
2015-02-10 23:08:03 +0800

    trapped

--- 2015/kb1/c.cc
+++ 2015/kb1/c.cc
@@ -23,26 +23,6 @@ void bfs() {
             q.push(make_pair(a + 1, b + 1));
             q.push(make_pair(a * 2, b + 1));
         }
-
-        // if (t * 2 == k || t + 1 == k || t - 1 == k) {
-        //     printf("%d\n", p[t] + 1);
-        //     return;
-        // }
-        // // if (t > 0) {
-        // //     if (t * 2 < k && !p[t * 2])
-        // //         q.push(t * 2), p[t * 2] = p[t] + 1;
-        // //     if (!p[t - 1])
-        // //         q.push(t - 1), p[t - 1] = p[t] + 1;
-        // // }
-        // // if (t >= 0 && t < k && !p[t + 1])
-        // //     q.push(t + 1), p[t + 1] = p[t] + 1;
-        // if (t > k)
-        //     q.push(t - 1), p[t - 1] = p[t] + 1;
-        // else if (t * 2 < k)
-        //     q.push(t * 2), p[t * 2] = p[t] + 1;
-        // else {
-
-        // }
     }
 }
 int main() {
