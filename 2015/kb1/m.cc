e516afb29a1379602c2cff89825ab815d4d3a3ff
unisolate <unisolate@gmail.com>
2015-02-13 23:07:35 +0800

    bfs

--- 2015/kb1/m.cc
+++ 2015/kb1/m.cc
@@ -1,6 +1,85 @@
 #include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#define MX 128
 using namespace std;
-int main(){
-	
-	return 0;
+typedef struct {
+    int x, y, z, p;
+} S;
+int s, n, m, k, f[MX][MX][MX];
+void bfs() {
+    queue<S> q;
+    S t;
+    t.x = 0, t.y = 0, t.z = s, t.p = 0;
+    f[0][0][s] = 1;
+    q.push(t);
+    while (!q.empty()) {
+        t = q.front();
+        int u = t.x, v = t.y, w = t.z, a = t.p;
+        if ((u == s / 2 && u == v) || (v == s / 2 && v == w) || (w == s / 2
+                && w == u)) {
+            printf("%d\n", t.p);
+            return;
+        }
+        if (u) {
+            if (v < m) {
+                k = min(u, m - v);
+                if (!f[u - k][v + k][w]) {
+                    f[u - k][v + k][w] = 1;
+                    t.x = u - k, t.y = v + k, t.z = w, t.p = a + 1;
+                    q.push(t);
+                }
+            }
+            if (!f[0][v][w + u]) {
+                f[0][v][w + u] = 1;
+                t.x = 0, t.y = v, t.z = w + u, t.p = a + 1;
+                q.push(t);
+            }
+        }
+        if (v) {
+            if (u < n) {
+                k = min(v, n - u);
+                if (!f[u + k][v - k][w]) {
+                    f[u + k][v - k][w] = 1;
+                    t.x = u + k, t.y = v - k, t.z = w, t.p = a + 1;
+                    q.push(t);
+                }
+            }
+            if (!f[u][0][w + v]) {
+                f[u][0][w + v] = 1;
+                t.x = u, t.y = 0, t.z = w + v, t.p = a + 1;
+                q.push(t);
+            }
+        }
+        if (w) {
+            if (v < m) {
+                k = min(w, m - v);
+                if (!f[u][v + k][w - k]) {
+                    f[u][v + k][w - k] = 1;
+                    t.x = u, t.y = v + k, t.z = w - k, t.p = a + 1;
+                    q.push(t);
+                }
+            }
+            if (u < n) {
+                k = min(w, n - u);
+                if (!f[u + k][v][w - k]) {
+                    f[u + k][v][w - k] = 1;
+                    t.x = u + k, t.y = v, t.z = w - k, t.p = a + 1;
+                    q.push(t);
+                }
+            }
+        }
+        q.pop();
+    }
+    puts("NO");
+}
+int main() {
+    while (scanf("%d%d%d", &s, &n, &m), s) {
+        if (s % 2)
+            puts("NO");
+        else
+            memset(f, 0, sizeof(f)), bfs();
+    }
+    return 0;
 }
--- 2015/kb1/n.cc
+++ 2015/kb1/n.cc
@@ -1,6 +1,54 @@
 #include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#define MX 256
 using namespace std;
-int main(){
-	
-	return 0;
+typedef pair<int, int> P;
+int n, m, a, b, c, d, f[MX][MX], u[MX][MX], v[MX][MX];
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+char p[MX][MX];
+void bfs(int tx, int ty, int w[MX][MX]) {
+    queue<P> q;
+    q.push(make_pair(tx, ty));
+    f[tx][ty] = 1;
+    while (!q.empty()) {
+        P t = q.front();
+        q.pop();
+        int x = t.first, y = t.second;
+        for (int i = 0; i < 4; ++i) {
+            int xx = x + dx[i], yy = y + dy[i];
+            if (xx >= 0 && xx < n && yy >= 0 && yy < m && p[xx][yy] != '#'
+                    && p[xx][yy] != 'Y' && p[xx][yy] != 'M' && !f[xx][yy]) {
+                f[xx][yy] = 1;
+                w[xx][yy] = w[x][y] + 1;
+                q.push(make_pair(xx, yy));
+            }
+        }
+    }
+}
+int main() {
+    while (~scanf("%d%d", &n, &m)) {
+        memset(u, 0, sizeof(u));
+        memset(v, 0, sizeof(v));
+        for (int i = 0; i < n; ++i) {
+            scanf("%s", p[i]);
+            for (int j = 0; j < m; ++j) {
+                if (p[i][j] == 'Y')
+                    a = i, b = j;
+                if (p[i][j] == 'M')
+                    c = i, d = j;
+            }
+        }
+        memset(f, 0, sizeof(f)), bfs(a, b, u);
+        memset(f, 0, sizeof(f)), bfs(c, d, v);
+        int k = MX * MX * MX;
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < m; ++j)
+                if (p[i][j] == '@' && u[i][j] && v[i][j])
+                    k = min(k, u[i][j] + v[i][j]);
+        printf("%d\n", 11 * k);
+    }
+    return 0;
 }
