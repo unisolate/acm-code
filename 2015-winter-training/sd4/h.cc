847938feeea2052c40ded0426d53b6f731c2c0ca
unisolate <unisolate@gmail.com>
2015-05-11 23:46:50 +0800

    finally...

--- 2015-winter-training/sd4/h.cc
+++ 2015-winter-training/sd4/h.cc
@@ -1,6 +1,19 @@
 #include <cstdio>
+#define MX 50050
 using namespace std;
-int main(){
-	
-	return 0;
+int n, m, l, r, a, b;
+int p[MX];
+int main() {
+    int t, cas = 0;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d%d", &n, &m);
+        for (int i = 0; i < n; ++i)
+            scanf("%d", &p[i]);
+        printf("Case #%d:\n", ++cas);
+        while (m--) {
+            scanf("%d%d%d%d", &l, &r, &a, &b);
+        }
+    }
+    return 0;
 }
--- 2015-winter-training/sd4/j.cc
+++ 2015-winter-training/sd4/j.cc
@@ -1,6 +1,24 @@
 #include <cstdio>
 using namespace std;
-int main(){
-	
-	return 0;
+char name[99], a[99], b[99];
+int main() {
+    int t, n, x, y, mod;
+    long long s;
+    scanf("%d", &t);
+    while (t--) {
+        int k, p = 0;
+        scanf("%d%lld%d%d%d", &n, &s, &x, &y, &mod);
+        for (int i = 0; i < n; ++i) {
+            scanf("%s%s%d%s", name, a, &k, b);
+            while (p + k > s) {
+                printf("%lld pages for %s\n", s - p, name);
+                p = 0;
+                s = (s * x + y) % mod;
+            }
+            printf("%d pages for %s\n", k, name);
+            p += k;
+        }
+        if (t) puts("");
+    }
+    return 0;
 }
