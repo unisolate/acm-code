670fdc5af0faa91cefada1080792c41206d7bd34
unisolate <unisolate@gmail.com>
2015-08-27 16:49:34 +0800

    backup

--- sdu2014/1/B.cc
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-int main()
-{
-    long long n;
-    while (~scanf("%I64d", &n))
-        printf("%I64d\n\n", n * (n + 1) / 2);
-    return 0;
-}
--- white-book/trap.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    double i;
-    for (i=0;i!=10;i+=0.1)
-        printf ("%.1f\n",i);
-    return 0;
-}
