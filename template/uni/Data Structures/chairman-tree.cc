3cc3686812844c4c2313363d72fc1c10511587e4
Uni <unisolate@gmail.com>
2015-10-22 11:36:05 +0800

    trees

---
+++ template/uni/Data Structures/chairman-tree.cc
@@ -0,0 +1,64 @@
+#include <cstdio>
+#include <algorithm>
+#define MX 100010
+using namespace std;
+struct Node {
+    int l, r, v;
+} t[MX * 20];
+struct P {
+    int v, i;
+} p[MX];
+bool cmp(P a, P b) {
+    return a.v < b.v;
+}
+int n, cnt, pos[MX], root[MX];
+void U(int v, int &x, int l = 1, int r = n) {
+    t[cnt++] = t[x];
+    x = cnt - 1;
+    ++t[x].v;
+    if (l == r) {
+        return;
+    }
+    int m = (l + r) >> 1;
+    if (v <= m) {
+        U(v, t[x].l, l, m);
+    } else {
+        U(v, t[x].r, m + 1, r);
+    }
+}
+int Q(int L, int R, int k, int l = 1, int r = n) {
+    if (l == r) {
+        return l;
+    }
+    int v = t[t[R].l].v - t[t[L].l].v;
+    int m = (l + r) >> 1;
+    if (k <= v) {
+        return Q(t[L].l, t[R].l, k, l, m);
+    }
+    return Q(t[L].r, t[R].r, k - v, m + 1, r);
+}
+int main() {
+    int m;
+    t[0].l = t[0].r = t[0].v = root[0] = 0;
+    while (~scanf("%d%d", &n, &m)) {
+        for (int i = 1; i <= n; ++i) {
+            scanf("%d", &p[i].v);
+            p[i].i = i;
+        }
+        sort(p + 1, p + n + 1, cmp);
+        for (int i = 1; i <= n; ++i) {
+            pos[p[i].i] = i;
+        }
+        cnt = 1;
+        for (int i = 1; i <= n; ++i) {
+            root[i] = root[i - 1];
+            U(pos[i], root[i]);
+        }
+        while (m--) {
+            int l, r, k;
+            scanf("%d%d%d", &l, &r, &k);
+            printf("%d\n", p[Q(root[l - 1], root[r], k)].v);
+        }
+    }
+    return 0;
+}
---
+++ template/uni/Data Structures/partition-tree.cc
@@ -0,0 +1,72 @@
+#include <cstdio>
+#include <algorithm>
+#define MX 100010
+using namespace std;
+int p[MX], s[20][MX], sum[20][MX];
+void B(int l, int r, int n = 0) {
+    int m = (l + r) >> 1;
+    int L = l, R = m + 1;
+    int x = p[m];
+    sum[n][l] = 0;
+    int cnt = m - l + 1;
+    for (int i = l; i <= r; ++i) {
+        if (s[n][i] < x) {
+            --cnt;
+        }
+    }
+    for (int i = l; i <= r; ++i) {
+        if (i > l) {
+            sum[n][i] = sum[n][i - 1];
+        }
+        if (L <= m && (s[n][i] < x || (s[n][i] == x && cnt-- > 0))) {
+            s[n + 1][L++] = s[n][i];
+            sum[n][i]++;
+        } else {
+            s[n + 1][R++] = s[n][i];
+        }
+    }
+    if (l < m) {
+        B(l, m, n + 1);
+    }
+    if (m + 1 < r) {
+        B(m + 1, r, n + 1);
+    }
+}
+int Q(int L, int R, int k, int l, int r, int n = 0) {
+    if (l == r) {
+        return s[n][l];
+    }
+    int m = (l + r) >> 1;
+    int v = 0;
+    if (L > l) {
+        v = sum[n][L - 1];
+    }
+    int t = sum[n][R] - v;
+    if (t >= k) {
+        return Q(l + v, l + sum[n][R] - 1, k, l, m, n + 1);
+    }
+    return Q(m + 1 + L - l - v, m + 1 + R - l - sum[n][R], k - t, m + 1, r,
+             n + 1);
+}
+int main() {
+    int n, m;
+    while (~scanf("%d%d", &n, &m)) {
+        for (int i = 1; i <= n; ++i) {
+            scanf("%d", p + i);
+        }
+        for (int i = 0; i < 20; ++i) {
+            sum[i][0] = 0;
+        }
+        for (int i = 1; i <= n; ++i) {
+            s[0][i] = p[i];
+        }
+        sort(p + 1, p + n + 1);
+        B(1, n);
+        while (m--) {
+            int l, r, k;
+            scanf("%d%d%d", &l, &r, &k);
+            printf("%d\n", Q(l, r, k, 1, n));
+        }
+    }
+    return 0;
+}
